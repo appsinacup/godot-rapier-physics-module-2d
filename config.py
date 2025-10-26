b2584eb187b5475dde797a9249b91d0895bbb52e
Dragos Daian <daian.dragos@yahoo.com>
2025-10-26 11:38:19 +0100

    disable web rapier module

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 def can_build(env, platform):
     # Rapier doesn't support double precision
-    return env.get("precision", "single") != "double"
+    if env.get("precision", "single") == "double":
+        return False
+    # Right now Rapier is disabled on web
+    if platform == "web":
+        return False
+    return True
 
 
 def configure(env):
