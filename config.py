d158581945f031fc9f7e68738e0d316747b584c2
Dragos Daian <daian.dragos@yahoo.com>
2025-11-09 12:11:39 +0100

    Add docs (draft)

--- config.py
+++ config.py
@@ -7,3 +7,25 @@ def can_build(env, platform):
 
 def configure(env):
     pass
+
+def get_doc_classes():
+    return [
+        "Fluid2D",
+        "FluidEffect2D",
+        "FluidEffect2DElasticity",
+        "FluidEffect2DSurfaceTensionAKINCI",
+        "FluidEffect2DSurfaceTensionHE",
+        "FluidEffect2DSurfaceTensionWCSPH",
+        "FluidEffect2DViscosityArtificial",
+        "FluidEffect2DViscosityDFSPH",
+        "FluidEffect2DViscosityXSPH",
+        "RapierDirectBodyState2D",
+        "RapierDirectSpaceState2D",
+        "RapierMath2D",
+        "RapierPhysics2DExtensionLibrary",
+        "RapierPhysicsServer2D",
+        "RapierPhysicsServerFactory2D",
+    ]
+
+def get_doc_path():
+    return "doc_classes"
